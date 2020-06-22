f6ad8d59c992eabdf8e6b72e57e730c66ec9995d
David Widmann <dev+git@devmotion.de>
2020-06-22 17:34:44 +0200

    Simplify tests and do not use `eval`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,31 @@
-using ForwardDiff, Distributions, FiniteDifferences
-using Tracker, Zygote, ReverseDiff, Random, LinearAlgebra, PDMats
-using DistributionsAD, Test, LinearAlgebra, Combinatorics
-using ForwardDiff: Dual
-using StatsFuns: binomlogpdf, logsumexp
-const FDM = FiniteDifferences
-using DistributionsAD: TuringMvNormal, TuringMvLogNormal, TuringUniform
-using Distributions: meanlogdet
+using DistributionsAD
 
-include("test_utils.jl")
-include("distributions.jl")
-include("others.jl")
+using Combinatorics
+using Distributions
+using FiniteDifferences
+using ForwardDiff
+using PDMats
+using ReverseDiff
+using Tracker
+using Zygote
+
+using Random, LinearAlgebra, Test
+
+using Distributions: meanlogdet
+using DistributionsAD: TuringUniform, TuringMvNormal, TuringMvLogNormal,
+    TuringPoissonBinomial
+using ForwardDiff: Dual
+using StatsBase: entropy
+using StatsFuns: binomlogpdf, logsumexp
+
+const FDM = FiniteDifferences
+const GROUP = get(ENV, "GROUP", "All")
+
+if GROUP == "All" || GROUP == "Others"
+    include("others.jl")
+end
+
+if GROUP == "All" || GROUP == "AD"
+    include("ad/utils.jl")
+    include("ad/distributions.jl")
+end
