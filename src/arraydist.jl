f9bcc5dbad56e5450864986561013554f15ca8a9
David Widmann <dev+git@devmotion.de>
2020-08-01 00:41:08 +0200

    Remove some custom adjoints

--- src/arraydist.jl
+++ src/arraydist.jl
@@ -5,7 +5,8 @@ function summaporbroadcast(f, dists::AbstractArray, x::AbstractArray)
     return sum(map(f, dists, x))
 end
 function summaporbroadcast(f, dists::AbstractVector, x::AbstractMatrix)
-    return map(x -> summaporbroadcast(f, dists, x), eachcol(x))
+    # `eachcol` breaks Zygote, so we use `view` directly
+    return map(i -> summaporbroadcast(f, dists, view(x, :, i)), axes(x, 2))
 end
 @init @require LazyArrays = "5078a376-72f3-5289-bfd5-ec5146d43c02" begin
     function summaporbroadcast(f, dists::LazyArrays.BroadcastArray, x::AbstractArray)
@@ -32,14 +33,6 @@ end
 function Distributions.logpdf(dist::VectorOfUnivariate, x::AbstractMatrix{<:Real})
     # eachcol breaks Zygote, so we need an adjoint
     return summaporbroadcast(logpdf, dist.v, x)
-end
-ZygoteRules.@adjoint function Distributions.logpdf(
-    dist::VectorOfUnivariate,
-    x::AbstractMatrix{<:Real}
-)
-    # Any other more efficient implementation breaks Zygote
-    f(dist, x) = [sum(logpdf.(dist.v, view(x, :, i))) for i in 1:size(x, 2)]
-    return ZygoteRules.pullback(f, dist, x)
 end
 
 struct MatrixOfUnivariate{
@@ -80,9 +73,10 @@ Base.length(dist::VectorOfMultivariate) = length(dist.dists)
 function arraydist(dists::AbstractVector{<:MultivariateDistribution})
     return VectorOfMultivariate(dists)
 end
+
 function Distributions.logpdf(dist::VectorOfMultivariate, x::AbstractMatrix{<:Real})
-    # eachcol breaks Zygote, so we define an adjoint
-    return sum(map(logpdf, dist.dists, eachcol(x)))
+    # `eachcol` breaks Zygote, so we use `view` directly
+    return sum(map(logpdf, dist.dists, axes(x, 2)))
 end
 function Distributions.logpdf(dist::VectorOfMultivariate, x::AbstractArray{<:AbstractMatrix{<:Real}})
     return map(x -> logpdf(dist, x), x)
@@ -90,14 +84,7 @@ end
 function Distributions.logpdf(dist::VectorOfMultivariate, x::AbstractArray{<:Matrix{<:Real}})
     return map(x -> logpdf(dist, x), x)
 end
-ZygoteRules.@adjoint function Distributions.logpdf(
-    dist::VectorOfMultivariate,
-    x::AbstractMatrix{<:Real}
-)
-    return ZygoteRules.pullback(dist, x) do dist, x
-        sum(map(i -> logpdf(dist.dists[i], view(x, :, i)), 1:size(x, 2)))
-    end
-end
+
 function Distributions.rand(rng::Random.AbstractRNG, dist::VectorOfMultivariate)
     init = reshape(rand(rng, dist.dists[1]), :, 1)
     return mapreduce(i -> rand(rng, dist.dists[i]), hcat, 2:length(dist); init = init)
