7837fa698ddcf409a8f6d5e5df70c43a26583f31
David Widmann <devmotion@users.noreply.github.com>
2020-10-08 03:56:34 +0200

    AD packages are always required

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,6 +19,21 @@ Random.seed!(1) # Set seed that all testsets should reset to.
 const FDM = FiniteDifferences
 const GROUP = get(ENV, "GROUP", "All")
 
+# Figure out which AD backend to test
+const AD = get(ENV, "AD", "All")
+if AD == "All" || AD == "ForwardDiff"
+    @eval using ForwardDiff
+end
+if AD == "All" || AD == "Zygote"
+    @eval using Zygote
+end
+if AD == "All" || AD == "ReverseDiff"
+    @eval using ReverseDiff
+end
+if AD == "All" || AD == "Tracker"
+    @eval using Tracker
+end
+
 if GROUP == "All" || GROUP == "Others"
     include("others.jl")
 end
@@ -28,20 +43,7 @@ if GROUP == "All" || GROUP == "AD"
     to_posdef(A::AbstractMatrix) = A * A' + I
     to_posdef_diagonal(a::AbstractVector) = Diagonal(a.^2 .+ 1)
 
-    # Figure out which AD backend to test
-    const AD = get(ENV, "AD", "All")
-    if AD == "All" || AD == "ForwardDiff"
-        @eval using ForwardDiff
-    end
-    if AD == "All" || AD == "Zygote"
-        @eval using Zygote
-    end
-    if AD == "All" || AD == "ReverseDiff"
-        @eval using ReverseDiff
-    end
     if AD == "All" || AD == "Tracker"
-        @eval using Tracker
-
         @eval begin
             # Define adjoints for Tracker
             to_posdef(A::TrackedMatrix) = Tracker.track(to_posdef, A)
