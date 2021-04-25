c46396060564a1c565ba5e5031ba1a8788d6ed57
David Widmann <devmotion@users.noreply.github.com>
2021-04-25 20:07:53 +0200

    Update ChainRules definitions and add differential for PoissonBinomial pdf (#162)

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -11,15 +11,17 @@
     (c, -c, z),
 )
 
+# StatsFuns: https://github.com/JuliaStats/StatsFuns.jl/pull/106
+
 ## Beta ##
 
 @scalar_rule(
     betalogpdf(α::Real, β::Real, x::Number),
-    @setup(di = digamma(α + β)),
+    @setup(z = digamma(α + β)),
     (
-        @thunk(log(x) - digamma(α) + di),
-        @thunk(log(1 - x) - digamma(β) + di),
-        @thunk((α - 1)/x + (1 - β)/(1 - x)),
+        log(x) + z - digamma(α),
+        log1p(-x) + z - digamma(β),
+        (α - 1) / x + (1 - β) / (1 - x),
     ),
 )
 
@@ -27,10 +29,15 @@
 
 @scalar_rule(
     gammalogpdf(k::Real, θ::Real, x::Number),
+    @setup(
+        invθ = inv(θ),
+        xoθ = invθ * x,
+        z = xoθ - k,
+    ),
     (
-        @thunk(-digamma(k) - log(θ) + log(x)),
-        @thunk(-k/θ + x/θ^2),
-        @thunk((k - 1)/x - 1/θ),
+        log(xoθ) - digamma(k),
+        invθ * z,
+        - (1 + z) / x,
     ),
 )
 
@@ -38,48 +45,97 @@
 
 @scalar_rule(
     chisqlogpdf(k::Real, x::Number),
-    @setup(ko2 = k / 2),
-    (@thunk((-logtwo - digamma(ko2) + log(x)) / 2), @thunk((ko2 - 1)/x - one(ko2) / 2)),
+    @setup(hk = k / 2),
+    (
+        (log(x) - logtwo - digamma(hk)) / 2,
+        (hk - 1) / x - one(hk) / 2,
+    ),
 )
 
 ## FDist ##
 
 @scalar_rule(
-    fdistlogpdf(v1::Real, v2::Real, x::Number),
+    fdistlogpdf(ν1::Real, ν2::Real, x::Number),
     @setup(
-        temp1 = v1 * x + v2,
-        temp2 = log(temp1),
-        vsum = v1 + v2,
-        temp3 = vsum / temp1,
-        temp4 = digamma(vsum / 2),
+        xν1 = x * ν1,
+        temp1 = xν1 + ν2,
+        a = (x - 1) / temp1,
+        νsum = ν1 + ν2,
+        di = digamma(νsum / 2),
     ),
     (
-        @thunk((log(v1 * x) + 1 - temp2 - x * temp3 - digamma(v1 / 2) + temp4) / 2),
-        @thunk((log(v2) + 1 - temp2 - temp3 - digamma(v2 / 2) + temp4) / 2),
-        @thunk(v1 / 2 * (1 / x - temp3) - 1 / x),
+        (-log1p(ν2 / xν1) - ν2 * a + di - digamma(ν1 / 2)) / 2,
+        (-log1p(xν1 / ν2) + ν1 * a + di - digamma(ν2 / 2)) / 2,
+        ((ν1 - 2) / x - ν1 * νsum / temp1) / 2,
     ),
 )
 
 ## TDist ##
 
 @scalar_rule(
-    tdistlogpdf(v::Real, x::Number),
+    tdistlogpdf(ν::Real, x::Number),
+    @setup(
+        νp1 = ν + 1,
+        xsq = x^2,
+        invν = inv(ν),
+        a = xsq * invν,
+        b = νp1 / (ν + xsq),
+    ),
     (
-        @thunk((digamma((v + 1) / 2) - 1 / v - digamma(v / 2) - log(1 + x^2 / v) + x^2 * (v + 1) / v^2 / (1 + x^2 / v)) / 2),
-        @thunk(-x * (v + 1) / (v + x^2)),
-    )
+        (digamma(νp1 / 2) - digamma(ν / 2) + a * b - log1p(a) - invν) / 2,
+        - x * b,
+    ),
 )
 
 ## Binomial ##
 
 @scalar_rule(
-    binomlogpdf(n::Int, p::Real, x::Int),
-    (DoesNotExist(), x / p - (n - x) / (1 - p), DoesNotExist()),
+    binomlogpdf(n::Real, p::Real, k::Real),
+    @setup(z = digamma(n - k + 1)),
+    (
+        digamma(n + 2) - z + log1p(-p) - 1 / (1 + n),
+        (k / p - n) / (1 - p),
+        z - digamma(k + 1) + logit(p),
+    ),
 )
 
 ## Poisson ##
 
 @scalar_rule(
-    poislogpdf(v::Real, x::Int),
-    (x / v - 1, DoesNotExist()),
+    poislogpdf(λ::Number, x::Number),
+    ((iszero(x) && iszero(λ) ? zero(x / λ) : x / λ) - 1, log(λ) - digamma(x + 1)),
 )
+
+## PoissonBinomial
+
+function ChainRulesCore.rrule(
+    ::typeof(Distributions.poissonbinomial_pdf_fft), p::AbstractVector{<:Real}
+)
+    y = Distributions.poissonbinomial_pdf_fft(p)
+    A = poissonbinomial_partialderivatives(p)
+    function poissonbinomial_pdf_fft_pullback(Δy)
+        p̄ = InplaceableThunk(
+            @thunk(A * Δy),
+            Δ -> LinearAlgebra.mul!(Δ, A, Δy, true, true),
+        )
+        return (NO_FIELDS, p̄)
+    end
+    return y, poissonbinomial_pdf_fft_pullback
+end
+
+if isdefined(Distributions, :poissonbinomial_pdf)
+    function ChainRulesCore.rrule(
+        ::typeof(Distributions.poissonbinomial_pdf), p::AbstractVector{<:Real}
+    )
+        y = Distributions.poissonbinomial_pdf(p)
+        A = poissonbinomial_partialderivatives(p)
+        function poissonbinomial_pdf_pullback(Δy)
+            p̄ = InplaceableThunk(
+                @thunk(A * Δy),
+                Δ -> LinearAlgebra.mul!(Δ, A, Δy, true, true),
+            )
+            return (NO_FIELDS, p̄)
+        end
+        return y, poissonbinomial_pdf_pullback
+    end
+end
