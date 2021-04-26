8839f30dc79ca267eb068f8a6405f9aa0aea5d14
David Widmann <david.widmann@it.uu.se>
2021-04-26 23:09:14 +0200

    Load ForwardDiff correctly (hopefully)

--- src/DistributionsAD.jl
+++ src/DistributionsAD.jl
@@ -62,12 +62,17 @@ include("zygote.jl")
 
 @init begin
     @require ForwardDiff="f6369f11-7733-5829-9624-2563aa707210" begin
+        using .ForwardDiff
         using .ForwardDiff: @define_binary_dual_op # Needed for `eval`ing diffrules here
         include("forwarddiff.jl")
     end
 
     @require ReverseDiff = "37e2e3b7-166d-5795-8a7a-e32c996b4267" begin
-        include("reversediff.jl")
+        # ensures that we can load ForwardDiff without depending on it
+        # (it is a dependency of ReverseDiff and therefore always available)
+        @require ForwardDiff="f6369f11-7733-5829-9624-2563aa707210" begin
+            include("reversediff.jl")
+        end
     end
 
     @require Tracker="9f7883ad-71c0-57eb-9f7f-b5c9e6d3789c" begin
